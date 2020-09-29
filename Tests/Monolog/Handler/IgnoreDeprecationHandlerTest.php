0547bc9d855669902bdee85de93a1a3354cd065c
Thierry T <1940947+lepiaf@users.noreply.github.com>
2020-09-29 16:54:16 +0200

    EB-16486 add handler to ignore deprecation

---
+++ Tests/Monolog/Handler/IgnoreDeprecationHandlerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\ETSGlobal\LogBundle\Monolog\Handler;
+
+use ETSGlobal\LogBundle\Monolog\Handler\DeprecationHandler;
+use ETSGlobal\LogBundle\Monolog\Handler\IgnoreDeprecationHandler;
+use PHPUnit\Framework\TestCase;
+
+class IgnoreDeprecationHandlerTest extends TestCase
+{
+    public function provideData(): array
+    {
+        return [
+            [
+                [
+                    'channel' => 'php',
+                    'message' => 'User Deprecated: ',
+                ],
+                true,
+            ],
+            [
+                [
+                    'channel' => 'something else',
+                    'message' => 'User Deprecated: ',
+                ],
+                false,
+            ],
+            [
+                [
+                    'channel' => 'php',
+                    'message' => 'great log',
+                ],
+                false,
+            ],
+            [
+                [
+                ],
+                false,
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider provideData
+     */
+    public function testHandle(array $record, bool $expected): void
+    {
+        $handler = new IgnoreDeprecationHandler();
+        self::assertSame($expected, $handler->handle($record));
+    }
+}
