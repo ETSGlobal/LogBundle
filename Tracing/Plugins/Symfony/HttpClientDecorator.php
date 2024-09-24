16994c3b1d90470b14246197e0f7aee006d537d2
Alexandre Allier <aallier@eleven-labs.com>
2024-09-24 17:35:51 +0200

    fix: EB-0 Fix decorator

--- Tracing/Plugins/Symfony/HttpClientDecorator.php
+++ Tracing/Plugins/Symfony/HttpClientDecorator.php
@@ -5,14 +5,16 @@ declare(strict_types=1);
 namespace ETSGlobal\LogBundle\Tracing\Plugins\Symfony;
 
 use ETSGlobal\LogBundle\Tracing\TokenCollection;
+use Symfony\Component\HttpClient\DecoratorTrait;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
-use Symfony\Contracts\HttpClient\ResponseStreamInterface;
 
 /** Decorates a HttpClientInterface and injects the tracing token in the request.*/
 class HttpClientDecorator implements HttpClientInterface
 {
-    public function __construct(private HttpClientInterface $httpClient, private TokenCollection $tokenCollection)
+    use DecoratorTrait;
+
+    public function __construct(private HttpClientInterface $client, private TokenCollection $tokenCollection)
     {
     }
 
@@ -20,19 +22,6 @@ class HttpClientDecorator implements HttpClientInterface
     {
         $options['headers']['X-Token-Global'] = $this->tokenCollection->getTokenValue('global');
 
-        return $this->httpClient->request($method, $url, $options);
-    }
-
-    public function stream(ResponseInterface|iterable $responses, ?float $timeout = null): ResponseStreamInterface
-    {
-        return $this->httpClient->stream($responses, $timeout);
-    }
-
-    public function withOptions(array $options): static
-    {
-        $clone = clone $this;
-        $clone->httpClient = $this->httpClient->withOptions($options);
-
-        return $clone;
+        return $this->client->request($method, $url, $options);
     }
 }
